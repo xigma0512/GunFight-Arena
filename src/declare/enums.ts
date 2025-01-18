3aa3a3f5b7b7babf24c545dc9875c409d87562db
xig1517 <xig1517@gmail.com>
2025-01-19 00:04:20 +0800

    Feat: Types and Enums

---
+++ src/declare/enums.ts
@@ -0,0 +1,18 @@
+export enum Status {
+    Waiting,
+    Preparing,
+    Running,
+    GameOver
+}
+
+export enum GameMode {
+    DeathMatch,
+    Demolition // 爆破模式
+}
+
+export enum Team {
+    None,
+    Red,
+    Blue,
+    Spectator,
+}
---
+++ src/declare/types.d.ts
@@ -0,0 +1,9 @@
+import { Vector3 } from "@minecraft/server";
+
+declare type PropertyDataType = string | number | boolean | Vector3 | undefined
+
+declare interface IProperty {
+    readonly propertyId: string;
+    get value(): PropertyDataType
+    update(value: PropertyDataType): void
+}
