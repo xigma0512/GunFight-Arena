5a459adbc9c0f4462f836fa9c9d8a88978b31015
xig1517 <xig1517@gmail.com>
2025-01-19 00:26:26 +0800

    Modify: saturation

--- src/system/primary/saturation.ts
+++ src/system/primary/saturation.ts
@@ -2,7 +2,7 @@ import { world } from "@minecraft/server"
 
 const saturation = () => {
     world.getAllPlayers().forEach(player => {
-        player.addEffect('saturation', 20)
+        player.addEffect('saturation', 20, { amplifier: 1, showParticles: false })
     })
 }
 export { saturation }
