a63747c4ee5f7f760f46cfc2bd72ab4d06a4c6a6
xig1517 <xig1517@gmail.com>
2025-01-20 16:47:15 +0800

    Refactor: 將entityDie的內容移植到entityHealthChange

--- src/event/after/entityHealthChange.ts
+++ src/event/after/entityHealthChange.ts
@@ -2,6 +2,21 @@ import { world } from "@minecraft/server";
 import { EntityHealthChangedAfterEvent, GameMode, Player } from "@minecraft/server"
 
 import { revival } from "../../utils/revival";
+import { PropertyManager } from "../../property/_manager";
+import { Team } from "../../declare/enums";
+import PTeamScore from "../../property/world/team_score";
+
+function playerDead(player: Player) {
+    const team = PropertyManager.entity(player).get('team').value
+    const pscore = PropertyManager.world().get('team_score') as PTeamScore
+    if (team == Team.Blue) pscore.updateTeamScore(Team.Red, 1)
+    if (team == Team.Red) pscore.updateTeamScore(Team.Blue, 1)
+
+    player.getComponent('inventory')?.container?.clearAll();
+    player.setGameMode(GameMode.spectator);
+    player.sendMessage("You've been eliminated. Respawn in 5 seconds!")
+    revival(player);
+}
 
 abstract class entityHealthChange {
     static subscribe = () => {
@@ -11,12 +26,7 @@ abstract class entityHealthChange {
 
             const p = ev.entity as Player
             p.setSpawnPoint({ x: p.location.x, y: p.location.y, z: p.location.z, dimension: p.dimension })
-            if (ev.newValue <= 0) {
-                p.getComponent('inventory')?.container?.clearAll();
-                p.setGameMode(GameMode.spectator);
-                p.sendMessage("You've been eliminated. Respawn in 5 seconds!")
-                revival(p);
-            }
+            if (ev.newValue <= 0) playerDead(p);
         })
     }
     static unsubscribe = (ev: (args: EntityHealthChangedAfterEvent) => void) => world.afterEvents.entityHealthChanged.unsubscribe(ev)
