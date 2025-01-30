2b501939115af93f6d763894066058e35761b45f
xig1517 <xig1517@gmail.com>
2025-01-30 22:08:52 +0800

    refactor: 將effect內容移動到primary當中

--- src/system/primary.ts
+++ src/system/primary.ts
@@ -3,9 +3,11 @@ import Demolition from "./status/_demolition";
 import { PropertyManager } from "../property/_manager";
 
 import { EGameMode } from "../declare/enums";
+import { world } from "@minecraft/server";
 
 export default function primarySystemRun() {
     mode_manager();
+    giveEffect();
 }
 
 function mode_manager() {
@@ -13,4 +15,10 @@ function mode_manager() {
     switch (gameMode) {
         case EGameMode.Demolition: Demolition.instance.tick(); break;
     }
+}
+
+function giveEffect() {
+    world.getAllPlayers().forEach(player => {
+        player.addEffect('saturation', 20, { amplifier: 1, showParticles: false });
+    });
 }
--- src/system/primary/effects.ts
+++
@@ -1,8 +0,0 @@
-import { world } from "@minecraft/server"
-
-const effect = () => {
-    world.getAllPlayers().forEach(player => {
-        player.addEffect('saturation', 20, { amplifier: 1, showParticles: false })
-    })
-}
-export { effect }
