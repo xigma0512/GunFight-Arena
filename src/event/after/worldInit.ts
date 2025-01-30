18614f7a9c1b56420b8a2fdd22e663033de7e21d
xig1517 <xig1517@gmail.com>
2025-01-30 22:04:26 +0800

    refactor: 將`resetProp`移到`worldInit`來實作

--- src/event/after/worldInit.ts
+++ src/event/after/worldInit.ts
@@ -1,16 +1,19 @@
 import { world } from "@minecraft/server";
 import { DisplaySlotId, ScoreboardObjective, WorldInitializeAfterEvent } from "@minecraft/server"
 
-import { resetProp } from "../../utils/reset";
+import { PropertyManager } from "../../property/_manager";
+import { EGameMode, Team, Status } from "../../declare/enums";
+import PTeamScore from "../../property/world/team_score";
 
 export default abstract class worldInit {
     static subscribe = () => {
         return world.afterEvents.worldInitialize.subscribe(ev => {
-            resetProp()
+            resetProp();
 
             if (world.scoreboard.getObjective('main_weapon') === undefined) world.scoreboard.addObjective('main_weapon');
             if (world.scoreboard.getObjective('pistol') === undefined) world.scoreboard.addObjective('pistol');
             if (world.scoreboard.getObjective('team') === undefined) world.scoreboard.addObjective('team', 'TEAM');
+
             world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.List, { 'objective': world.scoreboard.getObjective('team') as ScoreboardObjective })
             world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.BelowName, { 'objective': world.scoreboard.getObjective('team') as ScoreboardObjective })
 
@@ -18,4 +21,11 @@ export default abstract class worldInit {
         })
     }
     static unsubscribe = (ev: (args: WorldInitializeAfterEvent) => void) => world.afterEvents.playerSpawn.unsubscribe(ev)
+}
+
+function resetProp() {
+    PropertyManager.world().get('game_mode').update(EGameMode.DeathMatch);
+    PropertyManager.world().get('game_status').update(Status.Waiting);
+    (PropertyManager.world().get('team_score') as PTeamScore).updateTeamScore(Team.Blue, 0);
+    (PropertyManager.world().get('team_score') as PTeamScore).updateTeamScore(Team.Red, 0);
 }
