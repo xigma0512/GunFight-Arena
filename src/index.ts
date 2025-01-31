e61084edc7c802d9fee2b5b46de5d057d4427334
xig1517 <xig1517@gmail.com>
2025-02-01 02:24:18 +0800

    fix: 延遲載入遊戲

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import eventListener from "./game/event/_handler";
 import { runInterval } from "./core/system/_run";
+import { world } from "@minecraft/server";
 
-eventListener();
-runInterval()
+const ev = world.afterEvents.worldInitialize.subscribe(() => {
+    eventListener();
+    runInterval();
+    world.afterEvents.worldInitialize.unsubscribe(ev);
+})
