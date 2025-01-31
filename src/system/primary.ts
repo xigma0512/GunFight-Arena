4ac0a493803e989e9e6d2e763cf49f5062f3d7bd
xig1517 <xig1517@gmail.com>
2025-01-31 18:21:11 +0800

    feat: add fn `inGame_detect`

--- src/system/primary.ts
+++ src/system/primary.ts
@@ -2,18 +2,19 @@ import Demolition from "./status/_demolition";
 
 import { PropertyManager } from "../property/_manager";
 
-import { EGameMode } from "../declare/enums";
+import { Mode } from "../declare/enums";
 import { world } from "@minecraft/server";
 
 export default function primarySystemRun() {
     mode_manager();
     giveEffect();
+    inGame_detect();
 }
 
 function mode_manager() {
     const gameMode = PropertyManager.world().get('game_mode').value
     switch (gameMode) {
-        case EGameMode.Demolition: Demolition.instance.tick(); break;
+        case Mode.Demolition: Demolition.instance.tick(); break;
     }
 }
 
@@ -21,4 +22,11 @@ function giveEffect() {
     world.getAllPlayers().forEach(player => {
         player.addEffect('saturation', 20, { amplifier: 1, showParticles: false });
     });
+}
+
+function inGame_detect() {
+    for (const pl of world.getAllPlayers()) {
+        if (pl.hasTag('inGame')) return Demolition.instance.addPlayer(pl);
+        if (!pl.hasTag('inGame')) return Demolition.instance.removePlayer(pl);
+    }
 }
