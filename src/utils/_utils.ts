7059fea701b121085eb64c3290277a5cac3e159d
xig1517 <xig1517@gmail.com>
2025-01-31 18:16:37 +0800

    feat: add fn `resetProp`, remove fn `getTeamAlive`

--- src/utils/_utils.ts
+++ src/utils/_utils.ts
@@ -1,8 +1,18 @@
-import { EntityInventoryComponent, GameMode, InputPermissionCategory, ItemLockMode, ItemStack, Player, world } from "@minecraft/server";
+import { EntityInventoryComponent, GameMode, InputPermissionCategory, ItemLockMode, ItemStack, Player } from "@minecraft/server";
 import { PropertyManager } from "../property/_manager";
 
-import { Team } from "../declare/enums";
+import { Mode, Status, Team } from "../declare/enums";
 import config from "../config";
+import Demolition from "../gamemode/demolition/_handler";
+import PTeamScore from "../property/world/team_score";
+
+
+export const resetProp = () => {
+    PropertyManager.world().get('game_mode').update(Mode.Demolition);
+    Demolition.instance.state = Status.Waiting;
+    (PropertyManager.world().get('team_score') as PTeamScore).updateTeamScore(Team.Blue, 0);
+    (PropertyManager.world().get('team_score') as PTeamScore).updateTeamScore(Team.Red, 0);
+}
 
 
 /* Reset player data */
@@ -10,13 +20,17 @@ export const resetPlayerData = (player: Player) => {
     clearInventory(player);
     setGameMode(player, 'adventure');
     tp2Lobby(player);
+    player.removeTag('inGame');
+    player.inputPermissions.setPermissionCategory(InputPermissionCategory.Movement, true);
 
     const prop = PropertyManager.entity(player);
-    for (const [propName, propObject] of Object.entries(prop.properties())) propObject.update();
+    for (const [_, propObject] of Object.entries(prop.properties())) {
+        propObject.update();
+    }
 }
 
 
-/* Reset round */
+/* respawn player */
 export const respawnPlayer = (player: Player) => {
     const team = PropertyManager.entity(player).get('team').value as Team;
     const palive = PropertyManager.entity(player).get('alive');
@@ -26,7 +40,7 @@ export const respawnPlayer = (player: Player) => {
     setGameMode(player, 'adventure');
     tp2TeamSpawn(player, team);
     palive.update(true);
-    player.addEffect('health_boost', 2000000, { amplifier: 9, showParticles: false });
+    player.addEffect('health_boost', 2000000, { amplifier: 4, showParticles: false });
     player.addEffect('instant_health', 2, { amplifier: 99 });
 
     const item = new ItemStack('feather');
@@ -47,19 +61,6 @@ export const setGameMode = (player: Player, mode: 'creative' | 'survival' | 'adv
         'adventure': GameMode.adventure,
         'spectator': GameMode.spectator
     }[mode]);
-
-
-/* get team alive */
-export const getTeamAlive = (team: Team) => {
-    let result = 0;
-    world.getAllPlayers().forEach(p => {
-        const pt = PropertyManager.entity(p).get('team').value as Team;
-        const palive = PropertyManager.entity(p).get('alive').value as boolean;
-
-        if (pt == team && palive) result++;
-    });
-    return result;
-}
 
 
 /* Teleport */
