6ddb93d757dc7335417e3b95c7a5383fe6396c70
xig1517 <xig1517@gmail.com>
2025-01-31 18:14:32 +0800

    fix: update entity property `alive` when player dead

--- src/event/after/entityHealthChange.ts
+++ src/event/after/entityHealthChange.ts
@@ -2,6 +2,7 @@ import { world } from "@minecraft/server";
 import { EntityHealthChangedAfterEvent, Player } from "@minecraft/server"
 
 import { setGameMode } from "../../utils/_utils";
+import { PropertyManager } from "../../property/_manager";
 
 abstract class entityHealthChange {
     static subscribe = () => {
@@ -13,6 +14,7 @@ abstract class entityHealthChange {
             p.setSpawnPoint({ x: p.location.x, y: p.location.y, z: p.location.z, dimension: p.dimension })
             if (ev.newValue <= 0) {
                 setGameMode(p, 'spectator');
+                PropertyManager.entity(p).get('alive').update(false);
                 p.sendMessage("You've been eliminated. You will Respawn in next round.");
             }
         })
