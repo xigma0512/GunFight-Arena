cf18e7da9bafd17d8ac21d1a6085b82833fb70e1
xig1517 <xig1517@gmail.com>
2025-01-31 18:22:05 +0800

    chore: rename `EGameMode` as `Mode`

--- src/property/world/game_mode.ts
+++ src/property/world/game_mode.ts
@@ -1,5 +1,5 @@
 import { IProperty } from "../../declare/types";
-import { EGameMode } from "../../declare/enums";
+import { Mode } from "../../declare/enums";
 import { world } from "@minecraft/server";
 
 export default class PGameMode implements IProperty {
@@ -8,9 +8,9 @@ export default class PGameMode implements IProperty {
 
     constructor() {
         if (world.getDynamicProperty(this.propertyId) === undefined)
-            world.setDynamicProperty(this.propertyId, EGameMode.Demolition)
+            world.setDynamicProperty(this.propertyId, Mode.Demolition)
     }
-    get value() { return world.getDynamicProperty(this.propertyId) as EGameMode }
-    update = (value = EGameMode.Demolition) => world.setDynamicProperty(this.propertyId, value);
+    get value() { return world.getDynamicProperty(this.propertyId) as Mode }
+    update = (value = Mode.Demolition) => world.setDynamicProperty(this.propertyId, value);
 }
 export { PGameMode }
