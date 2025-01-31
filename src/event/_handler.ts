fda2cbcc8384b014e34526bcbcefc80c92dc623b
xig1517 <xig1517@gmail.com>
2025-01-31 18:14:00 +0800

    feat: add event `playerLeave`

--- src/event/_handler.ts
+++ src/event/_handler.ts
@@ -2,12 +2,14 @@ import entityHealthChange from "./after/entityHealthChange";
 import itemUseOn from "./after/itemUseOn";
 import playerSpawn from "./after/playerSpawn";
 import worldInit from "./after/worldInit";
+import playerLeave from "./before/playerLeave";
 
 export default function eventListener() {
     [
         playerSpawn,
         worldInit,
         entityHealthChange,
-        itemUseOn
+        itemUseOn,
+        playerLeave
     ].forEach(ev => ev.subscribe())
 }
---
+++ src/event/before/playerLeave.ts
@@ -0,0 +1,12 @@
+import { PlayerLeaveBeforeEvent, world } from "@minecraft/server"
+import Demolition from "../../gamemode/demolition/_handler"
+
+abstract class playerLeave {
+    static subscribe = () => {
+        return world.beforeEvents.playerLeave.subscribe(ev => {
+            Demolition.instance.removePlayer(ev.player);
+        })
+    }
+    static unsubscribe = (ev: (args: PlayerLeaveBeforeEvent) => void) => world.beforeEvents.playerLeave.unsubscribe(ev)
+}
+export default playerLeave
