121db5fc1a81d050591927ec245075cbb9d57890
xig1517 <xig1517@gmail.com>
2025-02-01 18:37:46 +0800

    feat: set default spawn when worldInit

--- src/game/event/after/worldInit.ts
+++ src/game/event/after/worldInit.ts
@@ -2,10 +2,13 @@ import { world } from "@minecraft/server";
 import { WorldInitializeAfterEvent } from "@minecraft/server"
 import { Utils } from "../../../core/utils/utils";
 
+import config from "../../../config";
+
 export default abstract class worldInit {
     static subscribe = () => {
         return world.afterEvents.worldInitialize.subscribe(ev => {
             Utils.resetGameData();
+            world.setDefaultSpawnLocation(config.lobby_spawn);
             console.warn("world initialize");
         })
     }
