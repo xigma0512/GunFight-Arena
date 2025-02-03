bdf43e3c5797d44ee8ea2e5901956ce803ea23f4
xig1517 <xig1517@gmail.com>
2025-02-03 18:29:52 +0800

    refactor: 新增/調整 `config` 內容

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,25 @@ export default {
     },
     lobby_spawn: { x: -25, y: 30, z: -550 },
     demolition: {
-        spawn_point: {
-            blue_team: { x: -23, y: 26, z: -441 },
-            red_team: { x: -133, y: 33, z: -476 }
+        timer: {
+            preparing: 20,
+            running: 120,
+            bombPlanted: 50,
+            gameover: 5,
+            sleeping: 5,
         },
-        bomb_point: [
-            { x: -16, y: 29, z: -502 },
-            { x: -21, y: 32, z: -414 }
-        ],
-        bomb_point_scale: 5,
-        bomb_timer: 40,
+        bomb: {
+            spawn_point: { x: -21, y: 32, z: -414 },
+            bomb_point: {
+                A: { x: -16, y: 29, z: -502 },
+                B: { x: -21, y: 32, z: -414 }
+            },
+            bomb_point_range: 5,
+        },
+        team_spawn_point: {
+            blue: { x: -23, y: 26, z: -441 },
+            red: { x: -133, y: 33, z: -476 }
+        },
         winningScore: 5
     }
 }
