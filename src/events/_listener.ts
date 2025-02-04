3b4c3971cc271e4c582861780c360cccc71d0bf3
xig1517 <xig1517@gmail.com>
2025-02-04 22:11:24 +0800

    feat: add event listener of `entitySpawn` and `playerInteractWithEntity`
    -  `entitySpawn`: 玩家把炸彈包丟出背包時，會變成 `dropped_bomb` 的狀態，而非掉落物的狀態
    - `playerInteractWithEntity`: 玩家對 `dropped_bomb` 點擊右鍵，便可撿起炸彈包

--- src/events/_listener.ts
+++ src/events/_listener.ts
@@ -1,10 +1,12 @@
 import entityDie from "./after/entity/entityDie";
 import entityHealthChange from "./after/entity/entityHealthChange";
+import entitySpawn from "./after/entity/entitySpawn";
 
 import itemCompleteUse from "./after/item/itemCompleteUse";
 import itemUse from "./after/item/itemUse";
 
 import playerSpawn from "./after/player/playerSpawn";
+import playerInteractWithEntity from "./before/PlayerInteractWithEntity";
 
 import worldInit from "./after/worldInit";
 import ScriptMessage from "./after/scriptMessage";
@@ -13,11 +15,13 @@ export default function eventListener() {
     const events = [
         entityHealthChange,
         entityDie,
+        entitySpawn,
 
         itemUse,
         itemCompleteUse,
 
         playerSpawn,
+        playerInteractWithEntity,
 
         worldInit,
         ScriptMessage,
