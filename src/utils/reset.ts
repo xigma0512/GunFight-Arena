1e77ba25f792261203cf46fbd4eb2f1bfffab318
xig1517 <xig1517@gmail.com>
2025-02-07 22:24:10 +0800

    fix: 結束遊戲後玩家不會被傳送回大廳

--- src/utils/reset.ts
+++ src/utils/reset.ts
@@ -4,10 +4,13 @@ import { PlayerUtils } from "./player";
 import { Mode, States, Team } from "../declare/enums";
 import PTeamScore from "../property/world/team_score";
 
-import { Player } from "@minecraft/server";
+import { Player, world } from "@minecraft/server";
 
 export abstract class ResetUtils {
     static inGameData() {
+        for (const player of world.getAllPlayers()) {
+            ModeManager.getMode(Property.world().get('game_mode').value as number).removePlayer(player);
+        }
         Property.world().get('game_mode').update(Mode.Demolition);
         ModeManager.getMode(Mode.Demolition).setCurrentState(States.Demolition.Idle);
         (Property.world().get('team_score') as PTeamScore).updateTeamScore(Team.Blue);
@@ -15,19 +18,15 @@ export abstract class ResetUtils {
     }
 
     static playerData(player: Player) {
-        PlayerUtils.clearInventory(player);
-        PlayerUtils.setMovement(player, true);
-        PlayerUtils.setGameMode(player, 'adventure');
-        Property.entity(player).get('alive').update();
-
-        Property.entity(player).get('team').update(Team.None);
-        ModeManager.getMode(Property.world().get('game_mode').value as number).removePlayer(player);
-        PlayerUtils.tp2Spawn(player);
-
-        player.nameTag = player.name;
-
         for (const [_, propObject] of Object.entries(Property.entity(player).properties())) {
             propObject.update();
         }
+        PlayerUtils.clearInventory(player);
+        PlayerUtils.setMovement(player, true);
+        PlayerUtils.setGameMode(player, 'adventure');
+        
+        PlayerUtils.tp2Spawn(player);
+
+        player.nameTag = player.name;
     }
 }
