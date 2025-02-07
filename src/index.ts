8f874cbfa42bed2ce9028bf670591deeeb4ba251
xig1517 <xig1517@gmail.com>
2025-02-07 11:25:53 +0800

    refactor: 移除延遲載入

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import runTask from "./system/task/runTask";
 import eventListener from "./events/_listener";
-import { system } from "@minecraft/server";
 
 eventListener();
-system.runTimeout(runTask, 100);
+runTask();
