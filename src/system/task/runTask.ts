239036074dc0c6b14f174e2797c27e5268a735af
xig1517 <xig1517@gmail.com>
2025-02-08 14:21:56 +0800

    feat: 增加側邊欄信息

--- src/system/task/runTask.ts
+++ src/system/task/runTask.ts
@@ -4,8 +4,10 @@ import Property from "../../property/_handler";
 import ModeManager from "../../game/modes/_manager";
 import { DroppedBombHandler } from "../../game/bomb/droppedBomb";
 
-import { Mode } from "../../declare/enums";
+import { Mode, Team } from "../../declare/enums";
 import { EquipmentSlot, HudElement, world } from "@minecraft/server";
+import PTeamScore from "../../property/world/team_score";
+import { TeamUtils } from "../../utils/team";
 
 namespace PrimaryTask {
     export function GameTick() {
@@ -60,6 +62,70 @@ namespace RealTimeTask {
 
     export function UpdateInfoScreen() {
 
+        for (const player of world.getAllPlayers()) {
+            
+            const formatTime = (sec: number) => {
+                if (sec <= 0) return "0:00";
+                const minutes = Math.floor(sec / 60);
+                const remainingSeconds = sec % 60;
+                return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+            }
+            
+            const statusList = [
+                'Waiting for players...',
+                'Starting in seconds...',
+                'In Battle.            ',
+                'Bomb has been planted.',
+                'Round end.            ',
+                'GameOver.             '
+            ]
+            const pteam = Property.world().get('team_score') as PTeamScore
+            const [blueTeamScore, redTeamScore] = [pteam.getTeamScore(Team.Blue), pteam.getTeamScore(Team.Red)]
+            
+            const [blueTeamPlayer, redTeamPlayer] = [
+                TeamUtils.getPlayers(Team.Blue, world.getAllPlayers()),
+                TeamUtils.getPlayers(Team.Red, world.getAllPlayers()),
+            ];
+
+            let aliveText = " §l§f-----\n";
+            for (const player of blueTeamPlayer) {
+                const alive = Property.entity(player).get('alive').value;
+                aliveText += `  ${alive ? '§a+' : '§7-'} ${alive ? '§b' : '§7'}${player.name}\n`;
+            }
+
+            aliveText += " §l§f-----\n";
+            for (const player of redTeamPlayer) {
+                const alive = Property.entity(player).get('alive').value;
+                aliveText += `  ${alive ? '§a+' : '§7-'} ${alive ? '§c' : '§7'}${player.name}\n`;
+            }
+            aliveText += " §l§f-----";
+
+            const mode = ModeManager.getMode(Property.world().get('game_mode').value as Mode);
+
+            const rawMessage = {
+                "rawtext": [
+                    { "text": "§l§dGunfight Arena§r                  \n\n" },
+                    { "text": ` §eState:\n` },
+                    { "text": `  §e${statusList[mode.getCurrentState() as number]}\n\n` },
+                    { "text": ` §fScore: §b${blueTeamScore} §f| §c${redTeamScore} §f| §aTime: ${formatTime(mode.timer)}§r\n` },
+                    { "text": `\n` },
+                    { "text": aliveText }
+                ]
+            }
+
+            player.onScreenDisplay.setTitle(rawMessage);
+        }
+    }
+
+    export function StepSound() {
+        for(const player of world.getAllPlayers()) {
+            if (player.isSneaking) continue;
+            if (!player.isSprinting) continue; 
+            
+            world.getAllPlayers()
+                .filter(p=> p.name !== player.name)
+                .forEach(p=> p.playSound("step.stone", {location: player.location, volume: 2}));
+        }
     }
 }
 
@@ -71,7 +137,8 @@ export default function runTask() {
 
         [RealTimeTask.HandItemDetect, 1],
         [RealTimeTask.RotateDroppedBomb, 4],
-        [RealTimeTask.UpdateInfoScreen, 1]
+        [RealTimeTask.UpdateInfoScreen, 1],
+        [RealTimeTask.StepSound, 6]
     ];
 
     for (const [func, t] of tasks)
