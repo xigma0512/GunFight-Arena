9005a8d318a6fcb47798943e21a6b8fb90762fde
xig1517 <xig1517@gmail.com>
2025-02-09 21:50:45 +0800

    feat: 紀錄玩家拆包/裝包/擊殺/死亡

--- src/events/after/entity/entityDie.ts
+++ src/events/after/entity/entityDie.ts
@@ -2,6 +2,7 @@ import { EntityDieAfterEvent, Player, world } from "@minecraft/server"
 import Property from "../../../property/_handler"
 import { Team } from "../../../declare/enums";
 import { BroadcastUtils } from "../../../utils/broadcast";
+import PTempStat from "../../../property/entity/temp_stat";
 
 export default abstract class entityDie {
     static subscribe = () => {
@@ -10,14 +11,21 @@ export default abstract class entityDie {
 
             const deadEntity = ev.deadEntity as Player;
             if (deadEntity.typeId !== "minecraft:player") return;
-            const deadEntityTeam = Property.entity(deadEntity).get('team').value as Team
+
+            const deadEntityStat = Property.entity(deadEntity).get('temp_stat') as PTempStat;
+            deadEntityStat.updateStat('deaths', deadEntityStat.getStat('deaths') + 1);
+
+            const deadEntityTeam = Property.entity(deadEntity).get('team').value as Team;
 
             const killer = ev.damageSource.damagingEntity as Player;
             if (killer === undefined || killer.typeId !== "minecraft:player") {
                 BroadcastUtils.message(`§l§4DEAD ${teamTag(deadEntityTeam)}${deadEntity.name}`, 'message');
             } else {
-                const killerTeam = Property.entity(killer).get('team').value as Team
+                const killerTeam = Property.entity(killer).get('team').value as Team;
                 BroadcastUtils.message(`§l${teamTag(killerTeam)}${killer.name} §4KILLED ${teamTag(deadEntityTeam)}${deadEntity.name}`, 'message');
+
+                const killerStat = Property.entity(killer).get('temp_stat') as PTempStat;
+                killerStat.updateStat('kills', killerStat.getStat('kills') + 1);
             }
         })
     }
