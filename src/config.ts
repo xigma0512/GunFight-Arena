c27f967c8e1bf1dfc6fbc11a715065416c469362
xig1517 <xig1517@gmail.com>
2025-02-11 19:15:04 +0800

    refactor: remove `spawns` info in config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 export default {
-    special_item: {
-        container_position: { x: -66, y: 25, z: -538 },
+    container: {
+        position: { x: -66, y: 25, z: -538 },
         awp: 0,
         blue_armor: [1, 2, 3, 4],
         red_armor: [5, 6, 7, 8]
     },
-    lobby_spawn: { x: -25, y: 30, z: -550 },
     demolition: {
         timer: {
             preparation: 20,
@@ -15,16 +14,7 @@ export default {
             gameover: 5,
         },
         bomb: {
-            spawn_point: { x: -128, y: 33, z: -476 },
-            bomb_point: {
-                A: { x: -16, y: 29, z: -502 },
-                B: { x: -21, y: 32, z: -414 }
-            },
-            bomb_point_range: 5,
-        },
-        team_spawn_point: {
-            blue: { x: -23, y: 26, z: -441 },
-            red: { x: -133, y: 33, z: -476 }
+            bomb_target_range: 5,
         },
         winningScore: 5
     }
